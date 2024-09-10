a4f8b9a877d04fd12a5791cebf883513d8d61ef6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-10 19:07:51 +0100

    Updated StopWatch.jsx logic

--- src/assets/StopWatch.jsx
+++ src/assets/StopWatch.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function StopWatch() {
+
+    const [isRunning, setIsRunning] = useState(false);
+
+    useEffect(() => {
+
+    },[isRunning]);
+
+
+    function formatTime() {
+        return `00:00:00`;
+    }
+
     return(
-        <h2>Stop Watch</h2>
+        <div className="stopwatch">
+            <div className="display">
+                {formatTime()}
+            </div>
+        </div>
     )
 }
 
