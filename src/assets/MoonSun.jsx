f0739c8857d2c0775a9c8fbd0dc86a37ec7c0d7a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-10 14:14:11 +0100

    Updated MoonSun.jsx logic

--- src/assets/MoonSun.jsx
+++ src/assets/MoonSun.jsx
@@ -1,7 +1,22 @@
-import sun from './media/sunClock.png'
-import moon from './media/moonClock.png'
+import React, {useState, useEffect} from 'react';
+import sunDark from './media/sunClock.png';
+import sunLight from './media/sunClockLight.png';
+import moon from './media/moonClock.png';
 
 function MoonSun() {
+  
+  const [sun, setSun] = useState(sunLight);
+  const meridumSun = new Date().getHours();
+  const dayLight = meridumSun > 5 && meridumSun < 18 ? true : false;
+
+  useEffect(() => {
+    if (!dayLight) {
+      setSun(s => s = sunDark);
+    } else {
+      setSun(s => s = sunLight);
+    } 
+  },[meridumSun])
+
     return (
       <div className="moon-sun-container">
         <img src={moon} className="logo moon" alt="moon" />
