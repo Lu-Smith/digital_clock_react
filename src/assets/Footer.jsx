5262a6e4973a5df951c5c6d74e28c739655add12
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-10 13:23:03 +0100

    Created Footer.jsx

---
+++ src/assets/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer>
+            Coded by <a href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a>
+            , open-source <a href="https://github.com/Lu-Smith/digital_clock_react" target="_blank">GitHub</a>.
+        </footer>
+    )
+}
+
+export default Footer;
